cf155b001960a83f6833d4fc49e446df521e4e29
Nicholas Clark <nicholas.j.clark1214@gmail.com>
2022-04-24 19:32:46 +1000

    update man pages for new plot functions

---
+++ R/plot_mvgam_pterms.R
@@ -0,0 +1,90 @@
+#'Plot mvjagam parametric term partial effects
+#'
+#'This function plots posterior empirical quantiles for partial effects of parametric terms
+#'
+#'@param object \code{list} object returned from \code{mvjagam}
+#'@details Posterior empirical quantiles of each parametric term's partial effect estimates
+#'(on the link scale) are calculated and visualised as ribbon plots. These effects can
+#'be interpreted as the partial effect that a parametric term contributes when all other
+#'terms in the model have been set to \code{0}
+#'@return A base \code{R} graphics plot
+#'@export
+plot_mvgam_pterms = function(object){
+# General plotting colours and empirical quantile probabilities
+c_light <- c("#DCBCBC")
+c_light_trans <- c("#DCBCBC70")
+c_light_highlight <- c("#C79999")
+c_mid <- c("#B97C7C")
+c_mid_highlight <- c("#A25050")
+c_mid_highlight_trans <- c("#A2505095")
+c_dark <- c("#8F2727")
+c_dark_highlight <- c("#7C0000")
+probs = c(0.05, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.95)
+
+# Look for parametric terms in the model
+pterms <- attr(object$mgcv_model$pterms, 'term.labels')
+
+if(length(pterms) > 0){
+  # Graphical parameters
+  .pardefault <- par(no.readonly=T)
+  par(.pardefault)
+
+  if(length(pterms) == 1){
+    par(mfrow = c(1,1),
+        mar = c(4, 4.5, 3, 4))
+  }
+
+  if(length(pterms) == 2){
+    par(mfrow = c(2, 1))
+  }
+
+  if(length(pterms) %in% c(3, 4)){
+    par(mfrow = c(2, 2))
+  }
+
+  for(i in 1:length(pterms)){
+    # Find out which beta corresponds to the associated parametric term
+    betas_keep <- grepl(paste0(pterms[i]),
+                        colnames(predict(object$mgcv_model, type = 'lpmatrix')),
+                        fixed = T)
+    betas <- MCMCvis::MCMCchains(object$jags_output, 'b')[ ,betas_keep]
+    beta_creds <- quantile(betas, probs = probs)
+
+    # Generate linear predictor matrix from fitted mgcv model
+    Xp <- predict(object$mgcv_model, newdata = object$obs_data, type = 'lpmatrix')
+
+    # Zero out all other columns in Xp
+    Xp[,!betas_keep] <- 0
+
+    # X-axis values
+    pred_vals_orig <- sort(object$obs_data %>%
+      dplyr::pull(pterms[i]))
+    pred_vals <- seq(min(pred_vals_orig), max(pred_vals_orig), length.out = 500)
+    cred <- as.matrix(beta_creds) %*% pred_vals
+
+    # Plot
+    plot(1, type = "n",
+         xlab = pterms[i],
+         ylab = paste0('Partial effect of ', pterms[i]),
+         xlim = c(min(pred_vals), max(pred_vals)),
+         ylim = c(min(cred), max(cred)))
+    polygon(c(pred_vals, rev(pred_vals)), c(cred[1,], rev(cred[9,])),
+            col = c_light, border = NA)
+    polygon(c(pred_vals, rev(pred_vals)), c(cred[2,], rev(cred[8,])),
+            col = c_light_highlight, border = NA)
+    polygon(c(pred_vals, rev(pred_vals)), c(cred[3,], rev(cred[7,])),
+            col = c_mid, border = NA)
+    polygon(c(pred_vals, rev(pred_vals)), c(cred[4,], rev(cred[6,])),
+            col = c_mid_highlight, border = NA)
+    lines(pred_vals, cred[5,], col = c_dark, lwd = 2.5)
+    rug(pred_vals_orig,
+        lwd = 1.75, ticksize = 0.025, col = c_mid_highlight)
+
+  }
+
+  invisible()
+  par(.pardefault)
+} else {
+  stop('No parametric terms (apart from intercept) found in model')
+}
+}
---
+++ R/plot_mvgam_randomeffects.R
@@ -0,0 +1,128 @@
+#'Plot mvjagam random effect terms
+#'
+#'This function plots posterior empirical quantiles for random effect smooths (bs = re)
+#'
+#'@param object \code{list} object returned from \code{mvjagam}
+#'@details Posterior empirical quantiles of random effect coefficient estimates
+#'(on the link scale) are calculated and visualised as ribbon plots.
+#'Labels for coefficients are taken from the levels of the original factor variable
+#'that was used to specify the smooth in the model's formula
+#'@return A base \code{R} graphics plot
+#'@export
+plot_mvgam_randomeffects = function(object){
+
+# General plotting colours and empirical quantile probabilities
+c_light <- c("#DCBCBC")
+c_light_trans <- c("#DCBCBC70")
+c_light_highlight <- c("#C79999")
+c_mid <- c("#B97C7C")
+c_mid_highlight <- c("#A25050")
+c_mid_highlight_trans <- c("#A2505095")
+c_dark <- c("#8F2727")
+c_dark_highlight <- c("#7C0000")
+probs = c(0.05, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.95)
+
+# Labels of smooths in formula
+smooth_labs <- do.call(rbind, lapply(seq_along(object$mgcv_model$smooth), function(x){
+  data.frame(label = object$mgcv_model$smooth[[x]]$label,
+             class = class(object$mgcv_model$smooth[[x]])[1])
+}))
+
+# Check if any smooths were bs = "re"; if not, return an error
+if(any(smooth_labs$class == 'random.effect')){
+  re_smooths <- smooth_labs %>%
+    dplyr::filter(class == 'random.effect') %>%
+    dplyr::pull(label)
+
+  .pardefault <- par(no.readonly=T)
+  par(.pardefault)
+
+  if(length(re_smooths) == 1){
+    par(mfrow = c(1,1),
+        mar = c(4, 4.5, 3, 4))
+  }
+
+  if(length(re_smooths) == 2){
+    par(mfrow = c(2, 1))
+  }
+
+  if(length(re_smooths) %in% c(3, 4)){
+    par(mfrow = c(2, 2))
+  }
+
+  for(i in 1:length(re_smooths)){
+    # Find out which betas correspond to the associated random effect estimates
+    betas_keep <- grepl(paste0(re_smooths[i]),
+                        colnames(predict(object$mgcv_model, type = 'lpmatrix')),
+                        fixed = T)
+    betas <- MCMCvis::MCMCchains(object$jags_output, 'b')[ ,betas_keep]
+
+    # Plot the random effect estimates
+    beta_creds <- sapply(1:NCOL(betas),
+                          function(n) quantile(betas[,n],
+                                               probs = probs))
+    N <- NCOL(betas)
+    x <- 1:N
+    idx <- rep(1:N, each = 2)
+    repped_x <- rep(x, each = 2)
+    x <- sapply(1:length(idx),
+                function(k) if(k %% 2 == 0)
+                  repped_x[k] + min(diff(x))/2 else
+                    repped_x[k] - min(diff(x))/2)
+
+    plot(1, type = "n",
+         ylab = re_smooths[i],
+         xlab = '',
+         xlim = range(x),
+         xaxt = 'n',
+         ylim = range(c(as.vector(beta_creds))))
+    rect(xleft = x[seq(1, N*2, by = 2)],
+         xright = x[seq(2, N*2, by = 2)],
+         ytop =  as.numeric(c(beta_creds[9,])),
+         ybottom =  as.numeric(c(beta_creds[1,])),
+         col = c_light,
+         border = 'transparent')
+    rect(xleft = x[seq(1, N*2, by = 2)],
+         xright = x[seq(2, N*2, by = 2)],
+         ytop =  as.numeric(c(beta_creds[8,])),
+         ybottom =  as.numeric(c(beta_creds[2,])),
+         col = c_light_highlight,
+         border = 'transparent')
+    rect(xleft = x[seq(1, N*2, by = 2)],
+         xright = x[seq(2, N*2, by = 2)],
+         ytop =  as.numeric(c(beta_creds[7,])),
+         ybottom =  as.numeric(c(beta_creds[3,])),
+         col = c_mid,
+         border = 'transparent')
+    rect(xleft = x[seq(1, N*2, by = 2)],
+         xright = x[seq(2, N*2, by = 2)],
+         ytop =  as.numeric(c(beta_creds[6,])),
+         ybottom =  as.numeric(c(beta_creds[4,])),
+         col = c_mid_highlight,
+         border = 'transparent')
+
+    for (k in 1:(N)) {
+      lines(x = c(x[seq(1, N*2, by = 2)][k],x[seq(2, N*2, by = 2)][k]),
+            y = c(beta_creds[5,k], beta_creds[5,k]),
+            col = c_dark, lwd = 2)
+    }
+
+    abline(h=0, col="white", lty=1, lwd=3, xpd = F)
+    abline(h=0, col="black", lty=1, lwd=2, xpd = F)
+
+    # Label x-axis with the factor variable levels
+    factor_var_name <- tail(strsplit(gsub('\\)', '',
+                                          gsub('s\\(', '',
+                                               re_smooths[i])), ',')[[1]], 1)
+    axis(side = 1, at = 1:N,
+         labels = levels(object$obs_data %>%
+                           dplyr::pull(factor_var_name)))
+  }
+
+  invisible()
+  par(.pardefault)
+
+} else {
+  stop('No random effect smooths (bs = "re") in model formula')
+}
+}
