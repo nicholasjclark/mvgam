feb8ca097eff783ff52056dc8f8daf173fc7bdbd
Nicholas Clark <nicholas.j.clark1214@gmail.com>
2022-09-26 16:27:14 +1000

    more efficient mcmc checks

--- R/stan_checks.R
+++ R/stan_checks.R
@@ -3,14 +3,16 @@
 #' @param quiet Logical (verbose or not?)
 #' @details Utility function written by Michael Betancourt (https://betanalpha.github.io/)
 #' @noRd
-check_div <- function(fit, quiet=FALSE) {
-  sampler_params <- rstan::get_sampler_params(fit, inc_warmup=FALSE)
+check_div <- function(fit, quiet=FALSE, sampler_params) {
+  if(missing(sampler_params)){
+    sampler_params <- rstan::get_sampler_params(fit, inc_warmup=FALSE)
+  }
   divergent <- do.call(rbind, sampler_params)[,'divergent__']
   n = sum(divergent)
   N = length(divergent)
 
   if (!quiet) cat(sprintf('%s of %s iterations ended with a divergence (%s%%)\n',
-                            n, N, 100 * n / N))
+                            n, N, round(100 * n / N, 4)))
   if (n > 0) {
     if (!quiet) cat('*Try running with larger adapt_delta to remove the divergences\n')
     if (quiet) return(FALSE)
@@ -24,15 +26,18 @@ check_div <- function(fit, quiet=FALSE) {
 #' @param quiet Logical (verbose or not?)
 #' @details Utility function written by Michael Betancourt (https://betanalpha.github.io/)
 #' @noRd
-check_treedepth <- function(fit, max_depth = 10, quiet=FALSE) {
-  sampler_params <- rstan::get_sampler_params(fit, inc_warmup=FALSE)
+check_treedepth <- function(fit, max_depth = 10, quiet=FALSE,
+                            sampler_params) {
+  if(missing(sampler_params)){
+    sampler_params <- rstan::get_sampler_params(fit, inc_warmup=FALSE)
+  }
   treedepths <- do.call(rbind, sampler_params)[,'treedepth__']
   n = length(treedepths[sapply(treedepths, function(x) x == max_depth)])
   N = length(treedepths)
 
   if (!quiet)
     cat(sprintf('%s of %s iterations saturated the maximum tree depth of %s (%s%%)\n',
-                  n, N, max_depth, 100 * n / N))
+                  n, N, max_depth, round(100 * n / N, 4)))
 
   if (n > 0) {
     if (!quiet) cat('*Run again with max_treedepth set to a larger value to avoid saturation\n')
@@ -47,15 +52,18 @@ check_treedepth <- function(fit, max_depth = 10, quiet=FALSE) {
 #' @param quiet Logical (verbose or not?)
 #' @details Utility function written by Michael Betancourt (https://betanalpha.github.io/)
 #' @noRd
-check_energy <- function(fit, quiet=FALSE) {
-  sampler_params <- rstan::get_sampler_params(fit, inc_warmup=FALSE)
+check_energy <- function(fit, quiet=FALSE, sampler_params) {
+  if(missing(sampler_params)){
+    sampler_params <- rstan::get_sampler_params(fit, inc_warmup=FALSE)
+  }
   no_warning <- TRUE
   for (n in 1:length(sampler_params)) {
     energies = sampler_params[n][[1]][,'energy__']
     numer = sum(diff(energies)**2) / length(energies)
     denom = var(energies)
     if (numer / denom < 0.2) {
-      if (!quiet) cat(sprintf('Chain %s: E-FMI = %s', n, numer / denom))
+      if (!quiet) cat(sprintf('Chain %s: E-FMI = %s', n,
+                              round(numer / denom, 4)))
       no_warning <- FALSE
     }
   }
@@ -73,8 +81,11 @@ check_energy <- function(fit, quiet=FALSE) {
 #' @param quiet Logical (verbose or not?)
 #' @details Utility function written by Michael Betancourt (https://betanalpha.github.io/)
 #' @noRd
-check_n_eff <- function(fit, quiet=FALSE) {
-  fit_summary <- rstan::summary(fit, probs = c(0.5))$summary
+check_n_eff <- function(fit, quiet=FALSE, fit_summary) {
+  if(missing(fit_summary)){
+    fit_summary <- rstan::summary(fit, probs = c(0.5))$summary
+  }
+
   if(any(grep('LV', rownames(fit_summary)))){
     fit_summary <- fit_summary[-grep('LV', rownames(fit_summary)), ]
     fit_summary <- fit_summary[-grep('lv_coefs', rownames(fit_summary)), ]
@@ -85,26 +96,19 @@ check_n_eff <- function(fit, quiet=FALSE) {
 
   iter <- dim(rstan:::extract(fit)[[1]])[[1]]
 
+  neffs <- fit_summary[,'n_eff']
+  ratios <- neffs / iter
   no_warning <- TRUE
-  for (n in 1:N) {
-    if(is.nan(fit_summary[,'n_eff'][n])){
-      ratio <- 1
-    } else {
-      ratio <- fit_summary[,'n_eff'][n] / iter
-    }
-    if (ratio < 0.001) {
-      if (!quiet) cat(sprintf('n_eff / iter for parameter %s is %s!\n',
-                                rownames(fit_summary)[n], ratio))
-      no_warning <- FALSE
-    }
-
-  }
+  if(min(ratios, na.rm = TRUE) < 0.001) no_warning <- FALSE
   if (no_warning) {
     if (!quiet) cat('n_eff / iter looks reasonable for all parameters\n')
     if (quiet) return(TRUE)
-  }
-  else {
-    if (!quiet) cat('*n_eff / iter below 0.001 indicates that effective sample size has likely been overestimated\n')
+  } else {
+    if (!quiet){
+      cat('n_eff / iter below 0.001 found for',
+          length(which(ratios < 0.001)),
+          'parameters\n*Effective sample size is likely overestimated for these parameters\n')
+    }
     if (quiet) return(FALSE)
   }
 }
@@ -114,8 +118,11 @@ check_n_eff <- function(fit, quiet=FALSE) {
 #' @param quiet Logical (verbose or not?)
 #' @details Utility function written by Michael Betancourt (https://betanalpha.github.io/)
 #' @noRd
-check_rhat <- function(fit, quiet=FALSE) {
-  fit_summary <- rstan::summary(fit, probs = c(0.5))$summary
+check_rhat <- function(fit, quiet=FALSE, fit_summary) {
+  if(missing(fit_summary)){
+    fit_summary <- rstan::summary(fit, probs = c(0.5))$summary
+  }
+
   if(any(grep('LV', rownames(fit_summary)))){
     fit_summary <- fit_summary[-grep('LV', rownames(fit_summary)), ]
     fit_summary <- fit_summary[-grep('lv_coefs', rownames(fit_summary)), ]
@@ -126,7 +133,7 @@ check_rhat <- function(fit, quiet=FALSE) {
 
   no_warning <- TRUE
   rhats <- fit_summary[,'Rhat']
-  if(any(rhats > 1.05)) no_warning <- FALSE
+  if(max(rhats, na.rm = TRUE) > 1.05) no_warning <- FALSE
   if (no_warning) {
     if (!quiet) cat('Rhat looks reasonable for all parameters\n')
     if (quiet) return(TRUE)
@@ -146,24 +153,27 @@ check_rhat <- function(fit, quiet=FALSE) {
 #' @details Utility function written by Michael Betancourt (https://betanalpha.github.io/)
 #' @noRd
 check_all_diagnostics <- function(fit, quiet=FALSE, max_treedepth = 10) {
+  sampler_params <- rstan::get_sampler_params(fit, inc_warmup=FALSE)
+  fit_summary <- rstan::summary(fit, probs = c(0.5))$summary
   if (!quiet) {
-    check_n_eff(fit)
-    check_rhat(fit)
-    check_div(fit)
-    check_treedepth(fit, max_depth = max_treedepth)
-    check_energy(fit)
+    check_n_eff(fit, fit_summary = fit_summary)
+    check_rhat(fit, fit_summary = fit_summary)
+    check_div(fit, sampler_params = sampler_params)
+    check_treedepth(fit, max_depth = max_treedepth,
+                    sampler_params = sampler_params)
+    check_energy(fit, sampler_params = sampler_params)
   } else {
     warning_code <- 0
 
-    if (!check_n_eff(fit, quiet=TRUE))
+    if (!check_n_eff(fit, quiet=TRUE, fit_summary = fit_summary))
       warning_code <- bitwOr(warning_code, bitwShiftL(1, 0))
-    if (!check_rhat(fit, quiet=TRUE))
+    if (!check_rhat(fit, quiet=TRUE, fit_summary = fit_summary))
       warning_code <- bitwOr(warning_code, bitwShiftL(1, 1))
-    if (!check_div(fit, quiet=TRUE))
+    if (!check_div(fit, quiet=TRUE, sampler_params = sampler_params))
       warning_code <- bitwOr(warning_code, bitwShiftL(1, 2))
-    if (!check_treedepth(fit, quiet=TRUE))
+    if (!check_treedepth(fit, quiet=TRUE, sampler_params = sampler_params))
       warning_code <- bitwOr(warning_code, bitwShiftL(1, 3))
-    if (!check_energy(fit, quiet=TRUE))
+    if (!check_energy(fit, quiet=TRUE, sampler_params = sampler_params))
       warning_code <- bitwOr(warning_code, bitwShiftL(1, 4))
 
     return(warning_code)
