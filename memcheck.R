d247299820475063f9de71f2fe0c8e07c3a673a9
Nicholas Clark <uqnclar2@uq.edu.au>
2024-05-09 18:09:39 +1000

    more useful memcheck

--- memcheck.R
+++ memcheck.R
@@ -1,16 +1,14 @@
 devtools::load_all()
 library(testthat)
-test_that("run sim AR and VAR functions for memory checks", {
-  expect_no_error(capture_output(sim_mvgam(family = gaussian(),
-                                           trend_model = RW())))
-  expect_no_error(capture_output(sim_mvgam(family = gaussian(),
-                                           trend_model = AR(p = 1))))
-  expect_no_error(capture_output(sim_mvgam(family = gaussian(),
-                                           trend_model = AR(p = 2))))
-  expect_no_error(capture_output(sim_mvgam(family = gaussian(),
-                                           trend_model = AR(p = 3))))
-  expect_no_error(capture_output(sim_mvgam(family = gaussian(),
-                                           trend_model = VAR())))
-  expect_no_error(capture_output(sim_mvgam(family = gaussian(),
-                                           trend_model = VAR(cor = TRUE))))
-})
+capture_output(sim_mvgam(family = gaussian(),
+                         trend_model = RW()))
+capture_output(sim_mvgam(family = gaussian(),
+                         trend_model = AR(p = 1)))
+capture_output(sim_mvgam(family = gaussian(),
+                         trend_model = AR(p = 2)))
+capture_output(sim_mvgam(family = gaussian(),
+                         trend_model = AR(p = 3)))
+capture_output(sim_mvgam(family = gaussian(),
+                         trend_model = VAR()))
+capture_output(sim_mvgam(family = gaussian(),
+                         trend_model = VAR(cor = TRUE)))
