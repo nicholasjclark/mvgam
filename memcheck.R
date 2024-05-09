3a79793014eecf6202223f8357eb81937611ce75
Nicholas Clark <uqnclar2@uq.edu.au>
2024-05-09 18:31:37 +1000

    ensure all c++ functions are checked in memcheck

--- memcheck.R
+++ memcheck.R
@@ -1,5 +1,8 @@
 devtools::load_all()
 library(testthat)
+
+# Test the c++ functions, which can all be done using sim_mvgam()
+# and forecast()
 capture_output(sim_mvgam(family = gaussian(),
                          trend_model = RW()))
 capture_output(sim_mvgam(family = gaussian(),
@@ -12,3 +15,5 @@ capture_output(sim_mvgam(family = gaussian(),
                          trend_model = VAR()))
 capture_output(sim_mvgam(family = gaussian(),
                          trend_model = VAR(cor = TRUE)))
+fc <- forecast(mvgam:::mvgam_example1,
+               newdata = mvgam:::mvgam_examp_dat$data_test)
