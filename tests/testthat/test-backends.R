d087f89aa8407e0f5c1e50001caff0cba643603a
Nicholas Clark <nicholas.j.clark1214@gmail.com>
2024-08-29 14:59:04 +1000

    add small tests that variational backends work

---
+++ tests/testthat/test-backends.R
@@ -0,0 +1,28 @@
+context("backends")
+
+skip_on_cran()
+
+test_that("variationals are converted to stanfit appropriately", {
+  dat <- data.frame(y = rnorm(100, 0, 1),
+                    series = as.factor('series1'))
+  mod <- SM(mvgam(y ~ 1,
+               data = dat,
+               family = gaussian(),
+               algorithm = 'meanfield',
+               silent = 2))
+  expect_true(inherits(mod, 'mvgam'))
+
+  mod <- SM(mvgam(y ~ 1,
+               data = dat,
+               family = gaussian(),
+               algorithm = 'fullrank',
+               silent = 2))
+  expect_true(inherits(mod, 'mvgam'))
+
+  mod <- SM(mvgam(y ~ 1,
+               data = dat,
+               family = gaussian(),
+               algorithm = 'laplace',
+               silent = 2))
+  expect_true(inherits(mod, 'mvgam'))
+})
