6ea6246cd1d6ac4c59c693d372caf5d5198c5c7f
Nicholas Clark <uqnclar2@uq.edu.au>
2024-12-04 15:22:40 +1000

    clean up some citations

--- R/how_to_cite.R
+++ R/how_to_cite.R
@@ -88,7 +88,7 @@ how_to_cite.mvgam <- function(object, ...){
   citations[[1]] <- "Clark, NJ and Wells K (2022). Dynamic Generalized Additive Models (DGAMs) for forecasting discrete ecological time series. Methods in Ecology and Evolution, 14, 771-784. doi.org/10.1111/2041-210X.13974"
   citations[[2]] <- "Burkner, PC (2017). brms: An R Package for Bayesian Multilevel Models Using Stan. Journal of Statistical Software, 80(1), 1-28. doi:10.18637/jss.v080.i01"
   citations[[3]] <- "Wood, SN (2017). Generalized Additive Models: An Introduction with R (2nd edition). Chapman and Hall/CRC."
-  citations[[4]] <- "Wang W and Yan J (2021). Shape-Restricted Regression Splines with R Package splines2. Journal of Data Science, 19(3), 498-517. doi:10.6339/21-JDS1020 <https://doi.org/10.6339/21-JDS1020>."
+  citations[[4]] <- "Wang W and Yan J (2021). Shape-Restricted Regression Splines with R Package splines2. Journal of Data Science, 19(3), 498-517. doi:10.6339/21-JDS1020 https://doi.org/10.6339/21-JDS1020."
 
   # Any specials; first check whether this model used a VAR / VARMA process
   specials_text <- NULL
