f9a57e63d849006825d34461f075980bd8bf685b
Nicholas Clark <nicholas.j.clark1214@gmail.com>
2025-07-02 15:41:59 +1000

    begin modularizing some plotting functions

--- R/plot_mvgam_series.R
+++ R/plot_mvgam_series.R
@@ -43,23 +43,10 @@
 #'                      prop_trend = 0.6, phi = 10)
 #'plot_mvgam_series(data = sim_data$data_train, series = 'all')
 #'@export
-plot_mvgam_series = function(
-  object,
-  data,
-  newdata,
-  y = 'y',
-  lines = TRUE,
-  series = 1,
-  n_bins,
-  log_scale = FALSE
-) {
-  # Check arguments
-  if (!missing(object)) {
-    if (!(inherits(object, "mvgam"))) {
-      stop('argument "object" must be of class "mvgam"')
-    }
-  }
-
+plot_mvgam_series <- function(object, data, newdata, y =
+                                'y', lines = TRUE, series = 1, n_bins = NULL, log_scale =
+                                FALSE) {
+  # Validate series
   if (is.character(series)) {
     if (series != 'all') {
       stop(
@@ -83,248 +70,215 @@ plot_mvgam_series = function(
     }
   }
 
-  # Check variables in data / data_train
-  if (!missing("data")) {
-    data_train <- data
-  } else {
-    data_train <- rlang::missing_arg()
-  }
-
-  # Choose models over data if both supplied
+  # Extract training data
   if (!missing(object)) {
+    if (!(inherits(object, "mvgam"))) {
+      stop('argument "object" must be of class "mvgam"')
+    }
+
     if (!missing("data")) {
       warning('both "object" and "data" were supplied; only using "object"')
     }
-    data_train <- object$obs_data
 
+    data_train <- object$obs_data
     resp_terms <- as.character(terms(formula(object$call))[[2]])
     if (length(resp_terms) == 1) {
-      out_name <- as.character(terms(object$call)[[2]])
+      y <- as.character(terms(object$call)[[2]])
     } else {
       if (any(grepl('cbind', resp_terms))) {
-        resp_terms <- resp_terms[-grepl('cbind', resp_terms)]
-        out_name <- resp_terms[1]
+        resp_terms <- resp_terms[-grepl('cbind',
+                                        resp_terms)]
+        y <- resp_terms[1]
       }
     }
-    y <- out_name
-  }
-
-  if (!missing(data_train)) {
-    # Add series factor variable if missing
-    if (!inherits(data_train, 'list')) {
-      if (!'series' %in% colnames(data_train)) {
-        data_train$series <- factor('series1')
-      }
-
-      # Must be able to index by time; it is too dangerous to 'guess' as this could make a huge
-      # impact on resulting estimates / inferences
-      if (!'time' %in% colnames(data_train)) {
-        stop('data does not contain a "time" column')
-      }
-    }
-
-    if (inherits(data_train, 'list')) {
-      if (!'series' %in% names(data_train)) {
-        data_train$series <- factor('series1')
-      }
-
-      if (!'time' %in% names(data_train)) {
-        stop('data does not contain a "time" column')
-      }
-    }
-  }
-
-  if (!as.character(y) %in% names(data_train)) {
-    stop(paste0('variable "', y, '" not found in data'), call. = FALSE)
   } else {
-    data_train$y <- data_train[[y]]
+    data_train <- data
   }
 
-  # Drop unused levels in data_train
-  data_train$series <- droplevels(data_train$series)
-
-  # Check variables in newdata / data_test
+  # Validate data
+  data_train <- validate_plot_data(data_train, y)
   if (!missing(newdata)) {
-    data_test <- newdata
-  } else {
-    data_test <- rlang::missing_arg()
+    data_test <- validate_plot_data(newdata, y)
   }
 
-  if (!missing(data_test)) {
-    # Add series factor variable if missing
-    if (!inherits(data_test, 'list')) {
-      if (!'series' %in% colnames(data_test)) {
-        data_test$series <- factor('series1')
-      }
+  # Determine what to plot
+  if (is.character(series) && series == 'all') {
 
-      # Must be able to index by time; it is too dangerous to 'guess' as this could make a huge
-      # impact on resulting estimates / inferences
-      if (!'time' %in% colnames(data_test)) {
-        stop('newdata does not contain a "time" column')
-      }
-    }
-
-    if (inherits(data_test, 'list')) {
-      if (!'series' %in% names(data_test)) {
-        data_test$series <- factor('series1')
-      }
-
-      if (!'time' %in% names(data_test)) {
-        stop('newdata does not contain a "time" column')
-      }
-    }
-
-    if (!y %in% names(data_test)) {
-      stop(paste0('variable "', y, '" not found in newdata'), call. = FALSE)
-    } else {
-      data_test$y <- data_test[[y]]
-    }
-
-    # Drop unused levels in data_train
-    data_test$series <- droplevels(data_test$series)
-  }
-
-  # only plot time series
-  if (series == 'all') {
-    # create tibble
+    # Only return a plot of time series
     dat <- dplyr::as_tibble(data_train) %>%
       dplyr::distinct(time, y, series)
+    plot_ts <- plot_time_series(dat, lines, log_scale, y,
+                                series)
+    return(plot_ts)
 
-    # log transform and y-axis labels
-    if (log_scale) {
-      dat$y <- log(dat$y + 1)
-      ylab <- paste0('log(', y, ' + 1)')
-    } else {
-      ylab <- paste0(y)
-    }
-
-    # create faceted time serires
-    plot_ts <- dat %>%
-      ggplot2::ggplot(ggplot2::aes(time, y)) +
-      ggplot2::facet_wrap(~series) +
-      ggplot2::labs(x = "Time", y = ylab) +
-      ggplot2::theme_bw()
-
-    if (lines) {
-      plot_ts <- plot_ts +
-        ggplot2::geom_line(colour = "#8F2727", linewidth = 0.75)
-    } else {
-      plot_ts <- plot_ts + ggplot2::geom_point(colour = "#8F2727")
-    }
-    plot_ts
-
-    # multiple plots for one time series
   } else {
-    # series name
-    s_name <- levels(data_train$series)[series]
 
-    # training data
+    # Return multiple plots for one time series
+    s_name <- levels(data_train$series)[series]
     dat <- dplyr::as_tibble(data_train) %>%
       dplyr::filter(series == s_name) %>%
       dplyr::distinct(time, y) %>%
       dplyr::mutate(data = "train")
-
-    # optionally bind test data
-    if (!missing(data_test)) {
+    if (!missing(newdata)) {
       dat <- dplyr::bind_rows(
         dat,
         dplyr::as_tibble(data_test) %>%
           dplyr::filter(series == s_name) %>%
           dplyr::distinct(time, y) %>%
-          dplyr::mutate(data = "test")
+          dplyr::mutate(data = "validate")
       )
     }
-
-    # log-transform y
-    if (log_scale) {
-      truth$y <- log(truth$y + 1)
-      ylab <- paste0('log(', y, ' + 1)')
-    } else {
-      ylab <- paste0(y)
-    }
-
-    # time series
-    if (!missing(data_test)) {
-      plot_ts <- dat %>%
-        ggplot2::ggplot(ggplot2::aes(time, y, colour = data)) +
-        ggplot2::geom_vline(
-          xintercept = dat %>%
-            dplyr::filter(data == "test") %>%
-            dplyr::pull(time) %>%
-            min(),
-          linetype = "dashed",
-          colour = "black"
-        ) +
-        ggplot2::scale_colour_manual(values = c("black", "#8F2727")) +
-        ggplot2::labs(title = "Time series", x = "Time", y = ylab) +
-        ggplot2::theme_bw()
-      if (lines) {
-        plot_ts <- plot_ts +
-          ggplot2::geom_line(show.legend = F, linewidth = 0.75)
-      } else {
-        plot_ts <- plot_ts + ggplot2::geom_point(show.legend = F)
-      }
-    } else {
-      plot_ts <- dat %>%
-        ggplot2::ggplot(ggplot2::aes(time, y)) +
-        ggplot2::labs(title = "Time series", x = "Time", y = ylab) +
-        ggplot2::theme_bw()
-      if (lines) {
-        plot_ts <- plot_ts +
-          ggplot2::geom_line(colour = "#8F2727", linewidth = 0.75)
-      } else {
-        plot_ts <- plot_ts + ggplot2::geom_point(colour = "#8F2727")
-      }
-    }
-
-    # histogram
-    if (missing(n_bins)) {
-      n_bins <- max(c(length(hist(c(dat$y), plot = F)$breaks), 20))
-    }
-    plot_hist <- dat %>%
-      ggplot2::ggplot(ggplot2::aes(y)) +
-      ggplot2::geom_histogram(bins = n_bins, fill = "#8F2727", col = 'white') +
-      ggplot2::labs(title = "Histogram", x = ylab, y = "Count") +
-      ggplot2::theme_bw()
-
-    # ACF
-    acf_y <- acf(dat$y, plot = F, na.action = na.pass)
-    plot_acf <- data.frame(acf = acf_y$acf[,, 1], lag = acf_y$lag[, 1, 1]) %>%
-      ggplot2::ggplot(ggplot2::aes(x = lag, y = 0, yend = acf)) +
-      ggplot2::geom_hline(
-        yintercept = c(-1, 1) * qnorm((1 + 0.95) / 2) / sqrt(acf_y$n.used),
-        linetype = "dashed"
-      ) +
-      ggplot2::geom_hline(
-        yintercept = 0,
-        colour = "#8F2727",
-        linewidth = 0.25
-      ) +
-      ggplot2::geom_segment(colour = "#8F2727", linewidth = 1) +
-      ggplot2::labs(title = "ACF", x = "Lag", y = "Autocorrelation") +
-      ggplot2::theme_bw()
-
-    # ECDF plot
-    range_y <- range(dat$y, na.rm = T)
-    plot_ecdf <- data.frame(
-      x = seq(range_y[1], range_y[2], length.out = 100)
-    ) %>%
-      dplyr::mutate(y = ecdf(dat$y)(x)) %>%
-      ggplot2::ggplot(ggplot2::aes(x, y)) +
-      ggplot2::geom_line(colour = "#8F2727", linewidth = 0.75) +
-      ggplot2::scale_y_continuous(limits = c(0, 1)) +
-      ggplot2::labs(title = "CDF", x = ylab, y = "Empirical CDF") +
-      ggplot2::theme_bw()
-
-    # plot
-    patchwork::wrap_plots(
-      plot_ts,
-      plot_hist,
-      plot_acf,
-      plot_ecdf,
-      ncol = 2,
-      nrow = 2,
-      byrow = T
+    plot_ts <- plot_time_series(dat, lines, log_scale, y,
+                                series)
+    plot_hist <- plot_histogram(dat, y, n_bins)
+    plot_acf_obj <- plot_acf(dat)
+    plot_ecdf_obj <- plot_ecdf(dat, y)
+    return(
+      patchwork::wrap_plots(
+        plot_ts,
+        plot_hist,
+        plot_acf_obj,
+        plot_ecdf_obj,
+        ncol = 2,
+        nrow = 2,
+        byrow = TRUE
+      )
     )
   }
+}
+
+#' Helper function to validate and format input plotting data
+#' @noRd
+validate_plot_data <- function(data, y) {
+  # Check if data is not a list
+  if (!inherits(data, 'list')) {
+    # If 'series' column is missing, create a default factor
+    if (!'series' %in% colnames(data)) {
+      data$series <- factor('series1')
+    }
+    # If 'time' column is missing, stop with error
+    if (!'time' %in% colnames(data)) {
+      stop('data does not contain a "time" column',
+           call. = FALSE)
+    }
+  } else {
+    # If data is a list, check for 'series' and 'time' in names
+    if (!'series' %in% names(data)) {
+      data$series <- factor('series1')
+    }
+    if (!'time' %in% names(data)) {
+      stop('data does not contain a "time" column')
+    }
+  }
+
+  # Check if the outcome variable 'y' exists in data
+  if (!y %in% names(data)) {
+    stop(paste0('variable "', y, '" not found in data'),
+         call. = FALSE)
+  } else {
+    # Assign the outcome variable to a standard column 'y'
+    data$y <- data[[y]]
+  }
+
+  # Drop unused factor levels in 'series'
+  data$series <- droplevels(data$series)
+
+  # Return the validated and formatted data
+  return(data)
+}
+
+#' Function to generate time series plots
+#' @noRd
+plot_time_series <- function(dat, lines = TRUE, log_scale =
+                               FALSE, ylab = 'y', series = 'all') {
+  if (log_scale) {
+    dat$y <- log(dat$y + 1)
+    ylab <- paste0('log(', ylab, ' + 1)')
+  }
+  if (series == 'all') {
+    p <- ggplot2::ggplot(dat, ggplot2::aes(time, y)) +
+      ggplot2::facet_wrap(~series) +
+      ggplot2::labs(x = "Time", y = ylab) +
+      ggplot2::theme_bw()
+    if (lines) {
+      p <- p + ggplot2::geom_line(colour = "#8F2727",
+                                  linewidth = 0.75)
+    } else {
+      p <- p + ggplot2::geom_point(colour = "#8F2727")
+    }
+  } else {
+    p <- ggplot2::ggplot(dat, ggplot2::aes(time, y, colour = data)) +
+      ggplot2::labs(title = "Time series", x = "Time", y =
+                      ylab) +
+      ggplot2::geom_vline(
+        xintercept = dat %>%
+          dplyr::filter(data == "validate") %>%
+          dplyr::pull(time) %>%
+          min(c(., Inf)),
+        linetype = "dashed",
+        colour = "black"
+      ) +
+      ggplot2::scale_colour_manual(values = c("#8F2727", "black")) +
+      ggplot2::theme_bw()
+    if (lines) {
+      p <- p + ggplot2::geom_line(show.legend = F, linewidth = 0.75)
+    } else {
+      p <- p + ggplot2::geom_point(show.legend = F)
+    }
+  }
+  return(p)
+}
+
+#' Function to create histogram of observed values
+#' @noRd
+plot_histogram <- function(dat, ylab = 'y', n_bins = NULL) {
+  if (is.null(n_bins)) {
+    n_bins <- max(c(length(hist(c(dat$y), plot = F)$breaks),
+                    20))
+  }
+  ggplot2::ggplot(dat, ggplot2::aes(y)) +
+    ggplot2::geom_histogram(bins = n_bins, fill = "#8F2727",
+                            col = 'white') +
+    ggplot2::labs(title = "Histogram", x = ylab, y =
+                    "Count") +
+    ggplot2::theme_bw()
+}
+
+#' Function to compute and plot autocorrelation
+#' @noRd
+plot_acf <- function(dat) {
+  acf_y <- acf(dat$y, plot = F, na.action = na.pass)
+  data.frame(acf = acf_y$acf[,, 1], lag = acf_y$lag[, 1, 1]) %>%
+    ggplot2::ggplot(ggplot2::aes(x = lag, y = 0, yend =
+                                   acf)) +
+    ggplot2::geom_hline(
+      yintercept = c(-1, 1) * qnorm((1 + 0.95) / 2) /
+        sqrt(acf_y$n.used),
+      linetype = "dashed"
+    ) +
+    ggplot2::geom_hline(
+      yintercept = 0,
+      colour = "#8F2727",
+      linewidth = 0.25
+    ) +
+    ggplot2::geom_segment(colour = "#8F2727", linewidth = 1) +
+    ggplot2::labs(title = "ACF", x = "Lag", y =
+                    "Autocorrelation") +
+    ggplot2::theme_bw()
+}
+
+#' Function to generate empirical cumulative distribution
+#' @noRd
+plot_ecdf <- function(dat, ylab = 'y') {
+  range_y <- range(dat$y, na.rm = T)
+  data.frame(x = seq(range_y[1], range_y[2], length.out =
+                       100)) %>%
+    dplyr::mutate(y = ecdf(dat$y)(x)) %>%
+    ggplot2::ggplot(ggplot2::aes(x, y)) +
+    ggplot2::geom_line(colour = "#8F2727", linewidth = 0.75) +
+    ggplot2::scale_y_continuous(limits = c(0, 1)) +
+    ggplot2::labs(
+      title = "CDF", x = ylab,
+      y = "Empirical CDF") +
+    ggplot2::theme_bw()
 }
